195a41ab32462f1e6958a20b1ba31952231cf492
redtreelchao <redtreelchao>
2016-07-21 10:23:19 +0800

    change baidu tongji code

--- spkid_admin/application/views/common/footer.php
+++ spkid_admin/application/views/common/footer.php
@@ -24,3 +24,12 @@ $(document).on('change', 'input[type=file]', function(e){
     }
 });
 </script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?2b7ada9e30cf3ff4319ef3cdeb47391c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
